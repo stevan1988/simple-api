38c507674b8587995bad0ccc13209d7a2571cc2c
steva1988 <stevan.todorovic@teaminformatics.com>
2018-11-02 11:20:14 +0100

    Adding Swagger configuration

--- src/main/java/com/todcode/api/ApiApplication.java
+++ src/main/java/com/todcode/api/ApiApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@Enable
 public class ApiApplication {
 
 	public static void main(String[] args) {
